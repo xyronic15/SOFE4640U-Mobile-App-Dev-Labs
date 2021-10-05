706002fb8db91254f0f3fe09afc3abd5bfda10a6
Janahan10 <janahan.ravichandran@gmail.com>
2021-10-05 12:38:16 -0400

    Added lab 2 starter code from canvas

---
+++ Lab 2/app/src/main/java/com/uoit/noteme/CreateNoteActivity.java
@@ -0,0 +1,21 @@
+package com.uoit.noteme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+
+        ImageView imageBack = findViewById(R.id.imageBack);
+        imageBack.setOnClickListener(v -> onBackPressed());
+
+        ImageView imageDone = findViewById(R.id.imageSave);
+        imageDone.setOnClickListener(v -> onBackPressed());
+    }
+}
